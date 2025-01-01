cafd5d2dd298ad583e8308a11b23bc30ee915eb1
PC <jupallyyashwanth376@gmail.com>
2025-01-01 21:00:37 +0530

    starter

---
+++ src/main/java/com/example/goodreadsData/GoodreadsDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodreadsData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodreadsDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodreadsDataApplication.class, args);
+	}
+
+}
